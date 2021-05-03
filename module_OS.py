d9487fa069c10901aaddf34f1090d01bf9712fc7
Akash <akashbharadwaj18062000@gmail.com>
2021-05-03 13:06:40 +0530

    last day

---
+++ module_OS.py
@@ -0,0 +1,19 @@
+import os
+# print(dir(os)) 
+
+print(os.getcwd())
+
+# # to change our working directiory
+# os.chdir("D://")
+# print(os.getcwd())
+
+# to check the files on current directiory
+# print(os.listdir())
+# print(os.listdir("D://"))
+
+# # To create a new folder in working Directiory
+# os.mkdir("This") # it make only one folder
+# os.makedirs("This/That") # it make folder inside a folder
+
+# to rename a file
+os.rename("Akash.txt" , "akash.txt")
